b570d66784d17fa3a8ad0916dc17bf519503f33e
sk <sk22@mailbox.org>
2025-06-08 21:13:52 +0200

    fix and restyle header line

--- src/features/status/components/Header.tsx
+++ src/features/status/components/Header.tsx
@@ -1,67 +1,40 @@
-import { Match, Show, splitProps, Switch, type ComponentProps } from "solid-js";
+import { Show, splitProps, type ComponentProps } from "solid-js";
+import { cx } from "~/utils/functions";
+import { getContentStatus } from "../utils";
+import HeaderLine from "./HeaderLine";
 import styles from "./Header.module.css";
-import RepeatFilled from "@fluentui/svg-icons/icons/arrow_repeat_all_16_filled.svg?component-solid";
-import ReplyFilled from "@fluentui/svg-icons/icons/arrow_reply_16_filled.svg?component-solid";
+import textStyles from "~/styles/text.module.css";
 import type { Entity } from "megalodon";
 
 export default function Header(
 	props: { status: Entity.Status } & ComponentProps<"header">,
 ) {
 	const [statusProps, headerProps] = splitProps(props, ["status"]);
-
-	const status = statusProps.status.reblog ?? statusProps.status;
-
-	const isThreadReply = () =>
-		statusProps.status.in_reply_to_account_id ===
-		statusProps.status.account.id;
-	const replyToAccount = () =>
-		statusProps.status.mentions.find(
-			(m) => m.id === statusProps.status.in_reply_to_account_id,
-		);
+	const contentStatus = () => getContentStatus(statusProps.status);
 
 	return (
 		<header class={styles.header} {...headerProps}>
 			<Show
 				when={
 					statusProps.status.reblog !== null ||
-					statusProps.status.in_reply_to_account_id !== null
+					contentStatus().in_reply_to_id !== null
 				}
 			>
-				<div class={styles.above}>
-					<Show when={statusProps.status.in_reply_to_id !== null}>
-						<ReplyFilled class={styles.reblogIcon} role="img" />
-						<span class={styles.reblogUsername}>
-							<Switch>
-								<Match when={isThreadReply()}>in thread</Match>
-								<Match when={replyToAccount() !== undefined}>
-									@{replyToAccount()?.acct}
-								</Match>
-							</Switch>
-						</span>
-					</Show>
-					<Show when={statusProps.status.reblog !== null}>
-						<RepeatFilled class={styles.reblogIcon} role="img" />
-						<img
-							class={styles.reblogAvatar}
-							src={statusProps.status.account.avatar}
-						/>
-						<span class={styles.reblogUsername}>
-							@{statusProps.status.account.acct}
-						</span>
-					</Show>
-				</div>
+				<HeaderLine status={statusProps.status} />
 			</Show>
-			<a class={styles.avatarLink} href={status.account.url}>
+			<a class={styles.avatarLink} href={contentStatus().account.url}>
 				<img
 					class={styles.avatar}
-					src={status.account.avatar}
-					alt={status.account.acct}
+					src={contentStatus().account.avatar}
+					alt={contentStatus().account.acct}
 				/>
 			</a>
-			<span class={styles.displayName}>
-				{status.account.display_name}
+			<span class={cx(styles.displayName, textStyles.ellipsis)}>
+				{contentStatus().account.display_name}
 			</span>
-			<span class={styles.username}>@{status.account.acct}</span>
+			<span class={cx(styles.username, textStyles.ellipsis)}>
+				@{contentStatus().account.acct}
+			</span>
 		</header>
 	);
 }
