0bcc4984d9b539fc44e9dc3d196a2299d7158e77
sk <sk22@mailbox.org>
2025-06-08 13:29:58 +0200

    fix attachments sizing

--- src/features/status/components/Attachments.tsx
+++ src/features/status/components/Attachments.tsx
@@ -8,7 +8,23 @@ export default function Attachments(props: { status: Entity.Status }) {
 			{props.status.media_attachments.map((att) => (
 				<Switch>
 					<Match when={att.type === "image"}>
-						<img src={att.url} alt={att.description ?? undefined} />
+						<img
+							src={att.preview_url ?? att.url}
+							alt={att.description ?? undefined}
+							title={att.description ?? undefined}
+							width={
+								att.meta?.small?.width ??
+								att.meta?.original?.width ??
+								att.meta?.width ??
+								undefined
+							}
+							style={{
+								"--_aspect-ratio":
+									att.meta?.original?.aspect ??
+									att.meta?.aspect ??
+									undefined,
+							}}
+						/>
 					</Match>
 					<Match when={att.type === "video" || att.type === "gifv"}>
 						<video
